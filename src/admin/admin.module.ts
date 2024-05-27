bd0dae2e0876d0f71c0fa02dd6139469794d6a9e
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-05-27 16:06:19 +0400

    remove

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AdminController } from './admin.controller';
 import { TokenModule } from 'src/token/token.module';
 
 @Module({
   imports: [TokenModule],
-  controllers: [AdminController],
 })
 export class AdminModule {}
--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { DashboardController } from './dashboard.controller';
 import { TokenModule } from 'src/token/token.module';
 
 @Module({
   imports: [TokenModule],
-  controllers: [DashboardController],
 })
 export class DashboardModule {}
